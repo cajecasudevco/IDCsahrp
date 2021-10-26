03ca891c26b313a1ae95c02b0b61949779937046
cjcarrer <cjcarrer@bancolombia.com.co>
2021-10-26 18:21:35 -0500

    Sin Mplementar Interface en la instancia de la clase filereader

---
+++ IDCsharp/IDCsharp.Tests/VideoService/FakeFileReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using IDCsharp.VideoService;
+
+
+namespace IDCsharp.Tests.VideoService
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
--- IDCsharp/IDCsharp/VideoService/FileReader.cs
+++ IDCsharp/IDCsharp/VideoService/FileReader.cs
@@ -4,8 +4,14 @@ using System.IO;
 
 namespace IDCsharp.VideoService
 {
-    public class FileReader
+    public interface IFileReader
     {
+        string Read(string path);
+    }
+
+    public class FileReader : IFileReader  {
+
+
         public string Read(string path)
         {
             return File.ReadAllText(path);
