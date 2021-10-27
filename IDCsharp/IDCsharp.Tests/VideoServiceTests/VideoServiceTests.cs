50ec666458c038d2a587a1e70344bbdf636732eb
cjcarrer <cjcarrer@bancolombia.com.co>
2021-10-26 21:21:55 -0500

    DAtos verdaderos

--- IDCsharp/IDCsharp.Tests/VideoServiceTests/VideoServiceTests.cs
+++ IDCsharp/IDCsharp.Tests/VideoServiceTests/VideoServiceTests.cs
@@ -8,18 +8,41 @@ namespace IDCsharp.Tests.VideoServiceTests
     [TestFixture]
     public class VideoServiceTests
     {
+
+        private VideoService.VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService.VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_return_Error()
         {
-            var mockFilereader = new Mock<IFileReader>();
-            mockFilereader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
 
-            var service = new VideoService.VideoService(mockFilereader.Object);
-
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("Error").IgnoreCase);
+
+        }
+        [Test]
+        public void ReadVideoTitle_DatosVerdaderos()
+        {
+
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("{ 'Id':11,'Title':'capitulo11','IsProcessed':false}");
+
+
+            var result = _videoService.ReadVideoTitle();
+                      
+
+            Assert.AreEqual(result, "capitulo11");
 
         }
 
